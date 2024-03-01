490d98d33114538cf90b46f05788a9f1812416b1
Sigrid Muller <sigrid.muller.vidal@gmail.com>
2024-03-01 17:23:36 -0300

    lesson 1 (routes)

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 //express web server
 const express = require('express');
 const app = express();
-const lesson1Controller = require('./controllers/lesson1');
-
-app.get('/', lesson1Controller.emilyRoute);
-app.get('/hannah', lesson1Controller.hannahRoute);
-
 const port = 3000;
+
+app.use('/', require('./routes'));
 
 app.listen(process.env.port || port);
 console.log('Web server listening on port ' + (process.env.port || port));
